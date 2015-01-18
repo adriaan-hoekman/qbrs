c5fc51036ddf47f65dc36106770fd1cc303fae00
David Z. Jiang <SharkIng@Shacas.com>
2015-01-18 13:21:59 -0500

    Add report function file

---
+++ lib/report.func.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Report functions file that allow user to get and ganerate report
+ */
+
+function get_report($search_item) {
+	// Search Report
+}
+
+function format_report() {
+	// This part we use XML ganerater to formate report information to a XML file
+}
+
+?>
