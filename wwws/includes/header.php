26958b4651ebdf44eb945269ffeff5a851bb7b83
David Z. Jiang <SharkIng@Shacas.com>
2015-01-19 17:17:17 -0500

    Fix the Image problem and Add Mobile Browser Control Test Code

--- wwws/includes/header.php
+++ wwws/includes/header.php
@@ -5,11 +5,13 @@
 </head>
 
 <body>
-<table cellspacing="20">
+<table cellspacing="20" align="center">
 	<tr>
 		<td>
 		<?php
-			echo '<img src=/QBRS/dev/wwws/images/Queens_logo.png width="192" height="146">';
+			$upOne = str_replace(realpath(dirname(__FILE__) . '/../../..'), '', realpath(dirname(__FILE__) . '/..'));
+			$imgLink = $upOne .'/images/Queens_logo.png';
+			echo '<img src="'. $imgLink .'" width="192" height="146">';
 		?>
 		</td>
         <td align="center">
@@ -22,3 +24,24 @@
   		</td>
 	</tr>
 </body>
+
+<?php
+	// Test version of different browser
+	$timestamp = time();
+	$user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+	//$via = $_GET['via'];
+
+	if($user_agent){
+    	$is_mobie = preg_match('/(Mobile|iPod|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP)/i', $user_agent);
+    	if($is_mobie){
+        	// Set Mobie Version
+        	setcookie('mtpl', 'True', $timestamp+86400 * 365, '/'); // Set Cookie
+    	}else{
+        	// Set Desktop Version
+        	setcookie('mtpl', 'False', $timestamp+86400 * 365, '/'); // Set Cookie
+    	}
+	}else{
+    	//exit('error: 400 no agent');
+	}
+	
+?>
