710334db94b6b27443e57ce0bf0121083ae6cb7e
Luke Dowker <11ld20@queensu.ca>
2015-03-03 15:45:02 -0500

    Non Reg users missing reports now being entered into database

--- lib/report.func.php
+++ lib/report.func.php
@@ -12,4 +12,29 @@ function format_report() {
 	// This part we use XML ganerater to formate report information to a XML file
 }
 
+function nonreg_submit_report($dbc, $date, $time, $location, $description, $returnlocation, $serialnumber) {
+  $bicycleid = array_values(mysqli_fetch_array($dbc->query("SELECT BicycleID FROM bicycle WHERE Serial = '$serialnumber'")))[0];
+  $mysql_date = date('Y-m-d',strtotime($date));
+  $mysql_time = date('G:i:s',strtotime($time));
+  switch ($returnlocation) {
+    case "security" :
+      $returnlocationnum = 1;
+      break;
+    case "parking" :
+      $returnlocationnum = 2;
+      break;
+    case "police" :
+      $returnlocationnum = 3;
+      break;
+    case "directContact" :
+      $returnlocationnum = 0;
+      break;
+  }
+  $result = mysqli_query($dbc,
+      "INSERT INTO report (Date, Time, Location, Description, ReturnLocation, BicycleID)
+                VALUES ('$date', '$time', '$location', '$description' ,'$returnlocationnum', '$bicycleid');")
+      or die ("<br />Couldn't execute query.");
+  return $result;
+}
+
 ?>
--- wwws/nonreg-report-confirm.php
+++ wwws/nonreg-report-confirm.php
@@ -3,42 +3,24 @@
   include_once '../lib/global.conf.php';
   include_once '../lib/reg.func.php';
   include_once '../lib/search.func.php';
+  include_once '../lib/report.func.php';
+  include_once '../lib/mail.func.php';
 ?>
 
 <nav>
 <h1>Missing Bicycle Report Confirmation</h1>
-<h2>Please enter detailed information about the found bicycle:</h2>
 </nav>
 
 <section>
   <?php
-    $serialnumber = $_POST['SerialNumber'];
-    $bikeModel = $_POST['BicycleModel'];
-    $bikeMake = $_POST['BicycleMake'];
-    $bikeDateFound = $_POST['DateFound'];
-    $bikeTimeFound = $_POST['TimeFound'];
-    $bikeLocationFound = $_POST['LocationFound'];
-    $bikeOtherInfo = $_POST['OtherInfo'];
-    $bikeReturnMethod = $_POST['ReturnMethod'];
-    $bikeContactField = $_POST['contactField'];
-
-    // echo $serialnumber;
-    // echo $bikeModel;
-    // echo $bikeMake;
-    // echo $bikeDateFound;
-    // echo $bikeTimeFound;
-    // echo $bikeLocationFound;
-    // echo $bikeOtherInfo;
-    // echo $bikeReturnMethod;
-    // echo $bikeContactField;
-
-    
 
     date_default_timezone_set("America/Toronto");
 
     $da = date("Y-m-d H:i:s");
+    missing_send_mail("11ld20@queensu.ca","Luke Dowker");
   ?>
-  
+  <h1>SUCCESS!</h1>
+
 </section>
 
 
