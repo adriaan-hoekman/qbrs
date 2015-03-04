3545346069835d43ed1a5129e17c54b860dca446
David Z. Jiang <SharkIng@Shacas.com>
2015-03-04 09:38:46 -0500

    Fix some conflit

--- lib/global.conf.php
+++ lib/global.conf.php
@@ -1,19 +1,23 @@
 <?php
 /*
  * Database Connection info
- * Author: @QCRS Group, CISC 498
+ * Author: @QBRS Group, CISC 498
  */
 
 // Define Database Connection
 //define('DB_HOST', Null);
-define('DB_PORT','4444');
-define('DB_FILE','/home/users/qbrssec/www-data/tmp/mysql.sock');
+//define('DB_PORT','4444');
+//define('DB_FILE','/home/users/qbrssec/www-data/tmp/mysql.sock');
 define('DB_USER','test');
 define('DB_PWD','testqbrs');
 define('DB_DBNAME','QBRS');
 
+$_SERVER['HTTP_QUEENSU_NETID'] = '1zwj';
+$_SERVER['HTTP_COMMON_NAME'] = 'David';
+$_SERVER['HTTP_QUEENSU_MAIL'] = 'SharkIng@Shacas.com';
+
 // Using MySQL
-$dbc = mysqli_connect(Null,DB_USER,DB_PWD,DB_DBNAME,DB_PORT,DB_FILE);
+$dbc = mysqli_connect('localhost',DB_USER,DB_PWD,DB_DBNAME,Null,Null);
 //$link = mysqli_connect(Null,'test','testqbrs','qbrs','4444','/home/users/qbrssec/www-data/tmp/mysql.sock');
 
 ?>
