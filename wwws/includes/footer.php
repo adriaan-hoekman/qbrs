bb678352535a441cfa68c86a9396be78b4fdaa42
Adriaan Hoekman <9ah67@queensu.ca>
2015-03-09 21:44:28 -0400

    Add more links to footer.

--- wwws/includes/footer.php
+++ wwws/includes/footer.php
@@ -4,9 +4,15 @@
 	<tr align="center"><td align="center"><b>Useful Links</b></td></tr>
 	<tr>
 		<td align="center" style="padding-bottom:5%">
+			<a href="http://www.cyclekingston.ca/" title="CYCLE Kingston" target="new">CYCLE Kingston</a>
+			<br/>
+			<a href="https://www.kpf.ca/start-report.html" title="Kingston Police" target="new">Kingston Police online reporting</a>
+			<br/>
+			<a href="http://queensu.ca/parking/regulations" title="Parking Regulations" target="new">Parking your bike on campus</a>
+			<br/>
 			<a href="http://youtu.be/JgHubY5Vw3Y" title="Video - How to properly lock your bicycle" target="new">Video - How to properly lock your bicycle</a>
 			<br/>
-			<a href="http://www.cyclekingston.ca/" title="CYCLE Kingston" target="new">CYCLE Kingston</a>
+			<a href="http://www.bicycling.com/news/featured-stories/smash-lab-bike-locks-broken" title="Lock Testing" target="new">Which bike locks can you trust?</a>
 			<br/>
 		</td>
 	</tr>
