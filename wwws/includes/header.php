6af32c6cca5a6d8112a5b01cd67570bc006e7698
Luke Dowker <11ld20@queensu.ca>
2015-03-11 20:36:52 -0400

    Attempt to add Favicon to website

--- wwws/includes/header.php
+++ wwws/includes/header.php
@@ -32,6 +32,10 @@
 	<!--<link rel="stylesheet" href="/pps/qbrs/includes/css/styles.css">-->
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="icon" 
+      type="image/png" 
+      href="../favicon.ico">
 		
   <title>Queen's Bicycle Registration System</title>
 </head>
