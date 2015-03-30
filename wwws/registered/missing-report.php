f1d4dae7843013bdae7e6e2ea0dd8f2a9aa53b18
David Z. Jiang <SharkIng@Shacas.com>
2015-03-30 14:10:44 -0400

    Location auto compl

--- wwws/registered/missing-report.php
+++ wwws/registered/missing-report.php
@@ -26,7 +26,7 @@
                 <tr><td><label class="col-lg-5 control-label">Bicycle: </label></td><td><div class="col-lg-20"><input class="form-control" name="id" value="<?php echo $_GET['serial']; ?>" disabled/></div></td></tr>
                 <tr><td><label class="col-lg-5 control-label">Date: </label></td><td><div class="col-lg-20"><input class="form-control" type="date" name="date"></input></div></td></tr>
                 <!--<tr><td><label class="col-lg-5 control-label">Time: </label></td><td><div class="col-lg-20"><input class="form-control" type="time" name="time"></input></div></td></tr>-->
-                <tr><td><label class="col-lg-5 control-label">Location: </label></td><div class="col-lg-15"><td><input class="form-control" name="location"></input></div></td></tr>
+                <tr><td><label class="col-lg-5 control-label">Location: </label></td><div class="col-lg-15"><td><input class="form-control" name="location" id="location"></input></div></td></tr>
                 <tr><td><label class="col-lg-5 control-label">Description: </label></td><div class="col-lg-15"><td><textarea class="form-control" name="desc" rows="10" cols=auto></textarea></div></td></tr>
             </table>
             </br>
@@ -41,6 +41,57 @@
 
 </div>
 </section>
+
+
+    <script>
+
+        var data = [
+        {"label" : "John Deutsch University Centre(JDUC)"},
+        {"label" : "Queen's Centre, including the Athletics and Recreation Centre (ARC)"},
+        {"label" : "Physical and Health Education Centre (PEC)"},
+        {"label" : "Dauglas Library"},
+        {"label" : "Joseph S. Stauffer Library"},
+        {"label" : "Goodes Hall"},
+        {"label" : "Dupuis Hall"},
+        {"label" : "Beamish-Munro Hall (ILC)"},
+        {"label" : "Goodwin Hall"},
+        {"label" : "Walter Light Hall"},
+        {"label" : "Robert Sutherland Hall"},
+        {"label" : "Sir John A. Macdonald Hall"},
+        {"label" : "Dunning Hall"},
+        {"label" : "Mackintosh-Corry Hall (Mac-Corry)"},
+        {"label" : "Ellis Hall"},
+        {"label" : "Jean Royce Hall"},
+        {"label" : "BioSciences Complex"},
+        {"label" : "McArthur Hall"},
+        {"label" : "An Clachan"},
+        {"label" : "Etherington Hall"},
+        {"label" : "Donald Gordon Centre"},
+        {"label" : "Waldron Tower"},
+        {"label" : "Watts Hall"},
+        {"label" : "Ban Righ Hall"},
+        {"label" : "Botterell Hall"},
+        {"label" : "Gordon Brockington Houses"},
+        {"label" : "Jeffery Hall"},
+        {"label" : "Kingston Hall"},
+        {"label" : "Leggett Hall"},
+        {"label" : "Leonard Hall"},
+        {"label" : "Morris Hall"},
+        {"label" : "Stirling Hall"},
+        {"label" : "Watson Hall"},
+        {"label" : "Cataraqui Building"},
+        {"label" : "Kingston Centre"},
+        {"label" : "Kingston Downtown"}
+        ];
+
+        $(function() {
+
+            $( "#location" ).autocomplete(
+        {
+            source:data
+        })
+});
+    </script>
 
 <?php
 	include_once '../includes/footer.php';
